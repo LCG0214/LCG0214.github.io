6bfd8a54fa702167c7762c49a18dda1384097f55
LCG0214 <71062757+LCG0214@users.noreply.github.com>
2024-03-19 01:27:25 +0000

    文章默认图片修改 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/18/hello-world/"];function toRandomPost(){
+var posts=["2024/03/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
