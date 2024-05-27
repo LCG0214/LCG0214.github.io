230b9989a2aa0c58689a6b8579ea4772b87d128f
LCG0214 <71062757+LCG0214@users.noreply.github.com>
2024-05-27 06:17:39 +0000

    feat：文章-Java跨Docker容器备份数据库数据 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/25/SpringCloud微服务之Docker篇/","2024/05/08/使用Docker部署Jenkins/","2024/03/22/易班答题助手使用手册/"];function toRandomPost(){
+var posts=["2024/05/25/Java跨Docker容器备份数据库数据/","2024/04/25/SpringCloud微服务之Docker篇/","2024/05/08/使用Docker部署Jenkins/","2024/03/22/易班答题助手使用手册/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
